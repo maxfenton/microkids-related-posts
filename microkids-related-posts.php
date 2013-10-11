4df02e8eeba83d95e763c97ad6154d8cee5d034e
Mehdi El Kettani <mehdi.el.kettani@aftonbladet.se>
2013-10-11 12:56:12 +0200

    Bumping the version for the plugin

--- microkids-related-posts.php
+++ microkids-related-posts.php
@@ -4,7 +4,7 @@ Plugin Name: Microkid's Related Posts
 Plugin URI: http://www.microkid.net/wordpress/related-posts/
 Description: Display a set of manually selected related items with your posts
 Author: Microkid
-Version: 4.0.3.3 
+Version: 4.0.5
 Author URI: http://www.superinteractive.com
 
 This software is distributed in the hope that it will be useful,
